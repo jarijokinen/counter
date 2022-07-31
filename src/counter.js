58316564334d6224eb5c35533c1949f664059d3b
Jari Jokinen <jari.jokinen@j2.fi>
2022-07-31 19:36:42 +0300

    Replace speed with duration

--- src/counter.js
+++ src/counter.js
@@ -2,21 +2,27 @@ export const counter = (options) => {
   const opts = {
     elements: document.querySelectorAll('[data-counter]'),
     delay: 500,
-    speed: 30,
+    duration: 2000,
+    fps: 60,
     ...options
   };
 
-  const animate = (el) => {
-    const value = parseInt(el.getAttribute('data-counter'));
-    const data = parseInt(el.innerText);
+  const steps = Math.round(opts.duration / (1000 / opts.fps));
+  const easeOutQuad = t => t * (2 - t);
 
-    if (data < value) {
-      el.innerText = data + 1;
-      setTimeout(animate, opts.speed, el);
-    }
-    else {
-      el.innerText = value;
-    }
+  const animate = (el) => {
+    let step = 0;
+    const value = parseInt(el.getAttribute('data-counter'));
+
+    const interval = setInterval(() => {
+      const count = Math.round(value * easeOutQuad(step / steps));
+
+      if (parseInt(el.innerText) != count) {
+        el.innerText = count;
+      }
+
+      (step++ == steps) && clearInterval(interval);
+    }, (1000 / opts.fps));
   };
   
   opts.elements.forEach((el) => {
